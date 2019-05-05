4d3fbbfaa8ebad12b07a2825b73f76cf71880edf
Brittany Javalera <brittjavalera@gmail.com>
2019-05-05 23:16:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -5,11 +5,15 @@ end
 def array_with_two_elements
   ["billie", "foxy"]
 end
+
 def first_element(taylor_swift)
   taylor_swift[0]
 end
+
 def third_element(taylor_swift)
   taylor_swift[2]
 end
+
 def last_element(taylor_swift)
-  taylor_swift.last
+  taylor_swift[-1]
+end
